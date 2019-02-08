c49009f1d716aba085ef8db70de096085c1bd10c
JohnWSMartel <jwm3464@g.rit.edu>
2019-02-07 20:30:25 -0500

    Push to save. Page was working has crashed again. Status code added unsure if they work. Not sure about other requirements.

---
+++ src/jsonResponses.js
@@ -0,0 +1,79 @@
+const respondJSON = (request, response, status, object) => {
+	response.writeHead(status, {'Content-Type': 'application/json'});
+	response.write(JSON.stringify(object));
+	response.end();
+};
+
+const success = (request, response) => {
+	const respondJSON = {
+		message: 'This is a successful response.',
+	};
+	
+	respondJSON(request, response, 200, responseJSON);
+};
+
+const badRequest = (request, response, params) => {
+	const responseJSON = {
+		message: 'This request has the required parameters.',
+	};
+	
+	if(!params.valid||params.valid !== 'true'){
+		responseJSON.message = 'Missing valid query parameter set to true.';
+		responseJSON.id = 'badRequest';
+		return respondJSON(request, response, 400, responseJSON);
+	};
+	
+	if(params.valid == 'true'){
+		responseJSON.message = 'Should not be true.';
+		responseJSON.id = 'badRequest';
+		return respondJSON(request, response, 200, responseJSON);
+	}
+	
+	return respondJSON(request, response, 200, responseJSON);
+};
+
+const unauthorized = (request, response, params) => {
+	const responseJSON = {
+		message: 'User does not have authorization.',
+	};
+	
+	if(!params.valid||params.valid !== 'yes'){
+		responseJSON.message = "Missing valid query parameter set to yes.";
+		responseJSON.id = 'unauthorized';
+		return respondJSON(request, response, 401, responseJSON);
+	}
+	else if (params.valid == 'yes'){
+		responseJSON.message = 'Should not be yes.',
+		response.id = 'unauthorized';
+		return respondJSON(request, response, 200, responseJSON);
+	}
+};
+
+const forbidden = (request, response, params) => {
+	const responseJSON = {
+		message: 'User is forbidden from seeing this page.',
+	};
+	return respondJSON(request, response, 403, responseJSON);
+};
+
+const notImplempented = (request, response, params) => {
+	const responseJSON = {
+		message: 'Feature has not been implemented.',
+	};
+	return respondJSON(request, response, 501, responseJSON);
+};
+
+const notFound = (request, response) => {
+	const responseJSON = {
+		message: 'The page you are looking for was not found.',
+		id: 'notFound',
+	};
+	
+	respondJSON(request, response, 404, responseJSON);
+};
+
+module.exports = {
+	success,
+	badRequest,
+	notFound,
+};
---
+++ src/server.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+	'/': htmlHandler.getIndex,
+	'/success':jsonHandler.success,
+	'/badRequest':jsonHandler.badRequest,
+	notFound:jsonHandler.notFound,
+};
+
+const onRequest = (request, response) => {	
+	console.log(request.url);
+	const parsedUrl = url.parse(request.url);
+	const params = query.parse(parsedUrl.query);
+	
+	if(urlStruct[parsedUrl.pathname]){
+		urlStruct[parsedUrl.pathname](request, response, params);
+	} else {
+		urlStruct.notFound(request, response, params);
+	}
+	
+	switch (request.method){
+		case 'GET':
+			if(parsedUrl.pathname === '/')
+			{
+				htmlHandler.getIndex(request, response);
+			}
+			else if(parsedUrl.pathname === '/style.css'){
+				htmlHandler.getCSS(request, response);
+			}
+			break;
+		default:
+			break;
+	}
+}
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
